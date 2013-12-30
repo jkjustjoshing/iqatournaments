32244ef9d0a386154f1df334f691d6bc94ee2bc3
Josh Kramer <josh.kramer@effectiveui.com>
2013-12-30 11:38:19 -0500

    Inital commit, bootstrapped using Yeoman with generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('iqatournamentsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
